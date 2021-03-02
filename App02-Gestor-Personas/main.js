e8fd2af42498296c96b81bfabe425904aa8738b4
Jimmy Espino <jimmyespino415@gmail.com>
2021-03-02 14:34:36 -0430

    App 02, preparacion de interfaz registro

---
+++ App02-Gestor-Personas/main.js
@@ -0,0 +1,45 @@
+const {app, BrowserWindow, Menu} = require('electron');
+
+let ventanaPrincipal;
+
+let menuAplicacionPlantilla =[
+    {
+        label: 'Aplicacion',
+        submenu:[
+            {
+                label: 'Acerca de',
+                click: () => {
+                    abrirVentanaAcercaDe();
+                }
+            }
+        ]
+    }
+];
+
+function crearVentanaPrincipal(){
+    ventanaPrincipal = new BrowserWindow({
+        width: 800,
+        height: 700,
+        webPreferences:{
+            nodeIntegration:true
+        }
+    });
+
+    ventanaPrincipal.loadFile('index.html');
+
+    let menu = Menu.buildFromTemplate(menuAplicacionPlantilla);
+    ventanaPrincipal.setMenu(menu);
+
+    ventanaPrincipal.on('closed',()=> {
+        ventanaPrincipal= null;
+    });
+
+
+}
+
+function abrirVentanaAcercaDe(){
+
+}
+
+
+app.whenReady().then(crearVentanaPrincipal);
